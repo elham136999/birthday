78b94c634aa2e00d192b880eb5d9792a099ff53e
elham136999 <elhamkorivand@yahoo.com>
2021-07-09 12:34:27 +0430

    add component

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [pepole, setpepole] = useState(data);
+  return (
+    <main>
+      <section className='container'>
+        <h3>{pepole.length} birthdays today</h3>
+        <List pepole={pepole} />
+        <button onClick={() => setpepole([])}>clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
