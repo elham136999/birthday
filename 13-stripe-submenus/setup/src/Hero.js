2b59a579372b5cdaed335e1bde6e1c69b610e506
elham136999 <elhamkorivand@yahoo.com>
2021-08-01 22:48:20 +0430

    add nav & hero

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,29 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  const { closesubmenu } = useGlobalContext();
 
-export default Hero
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>payments infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
+            in laying out print, graphic or web designs. The passage is
+            attributed to an unknown typesetter in the 15th century who is
+            thought to have scrambled parts of Cicero's De Finibus
+          </p>
+          <button className='btn'>start now</button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} alt='phone-img' className='phone-img' />
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,38 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+  const [isSubmenuOpen, setisSubmenuOpen] = useState(true);
+  const opensidebar = () => {
+    setIsSideBarOpen(true);
+  };
+  const closesidebar = () => {
+    setIsSideBarOpen(false);
+  };
+  const opensubmenu = () => {
+    isSubmenuOpen(true);
+  };
+  const closesubmenu = () => {
+    isSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSubmenuOpen,
+        isSideBarOpen,
+        opensidebar,
+        opensubmenu,
+        closesidebar,
+        closesubmenu,
+      }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
