262eb7cd57a1d28c5d750c218c2e6d89a04ed2c8
elham136999 <elhamkorivand@yahoo.com>
2021-08-13 15:55:43 +0430

    context

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h2 className='section-title'>about us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        temporibus nihil laborum deleniti numquam voluptatibus magnam, porro
+        error impedit optio eligendi omnis quidem nulla doloremque, laudantium
+        quasi vitae velit. Quod.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
