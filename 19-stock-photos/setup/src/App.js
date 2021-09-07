ea624a3ec4efc043a4d930731a1807a3f02a80f8
elham136999 <elhamkorivand@yahoo.com>
2021-09-07 22:53:02 +0430

    search-photo

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -12,36 +12,74 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
 
   const fetchImage = async () => {
     let url;
-    url = `${mainUrl}${clientID}`;
+    const urlpage = `&page=${page}`;
+    const urlquery = `&query=${query}`;
+    url = `${mainUrl}${clientID}${urlpage}`;
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlpage}${urlquery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlpage}`;
+    }
     setLoading(true);
 
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setPhotos(data);
+
+      setPhotos((oldphotos) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldphotos, data.results];
+        } else {
+          return [...oldphotos, ...data];
+        }
+      });
       setLoading(false);
-      console.log(data);
     } catch (error) {
       setLoading(false);
-      console.log(error);
     }
   };
   useEffect(() => {
     fetchImage();
+  }, [page]);
+  useEffect(() => {
+    const event = window.addEventListener("scroll", () => {
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        setPage((oldpage) => {
+          return oldpage + 1;
+        });
+      }
+    });
+
+    return () => window.removeEventListener("scroll", event);
   }, []);
 
   const handlesubmit = (e) => {
     e.preventDefault();
-    console.log("helliii");
+    setPage(1);
+    fetchImage();
   };
   return (
     <main>
       <section className='search'>
         <form action='' className='search-form'>
-          <input type='text' className='form-input' placeholder='search...' />
+          <input
+            type='text'
+            className='form-input'
+            placeholder='search...'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
           <button type='submit' className='submit-btn' onClick={handlesubmit}>
             <FaSearch />
           </button>
@@ -53,7 +91,7 @@ function App() {
             return <Photo key={image.id} {...image} />;
           })}
         </div>
-        {loading && <h2 className='loading'> LOADING</h2>}
+        {loading && <h2 className='loading'> LOADING...</h2>}
       </section>
     </main>
   );
