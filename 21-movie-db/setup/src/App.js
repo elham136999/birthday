71606a76879c526a98cb3069539615391fce096d
elham136999 <elhamkorivand@yahoo.com>
2021-09-14 22:29:45 +0430

    api

--- 21-movie-db/setup/src/App.js
+++ 21-movie-db/setup/src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route } from "react-router-dom";
 
 import Home from "./Home";
 import Movie from "./SingleMovie";
+export const API_ENDPOINT = `http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+console.log(API_ENDPOINT);
 
 function App() {
   return (
